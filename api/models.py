e10af649835169de4ef9bc4fbc4cd76435de07fe
jigneshdhami <jignesh.dhami@gmail.com>
2021-06-21 10:39:48 +0530

    Initial commit

---
+++ api/models.py
@@ -0,0 +1,33 @@
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=32)
+    description = models.TextField(max_length=360)
+
+    def no_of_ratings(self):
+        ratings = Rating.objects.filter(movie=self)
+        return len(ratings)
+
+    def avg_rating(self):
+        add = 0
+        ratings = Rating.objects.filter(movie=self)
+        for rating in ratings:
+            add += rating.stars
+
+        if len(ratings) > 0:
+            return add / len(ratings)
+        else:
+            return 0
+
+
+class Rating(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    stars = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+
+    class Meta:
+        unique_together = (('user', 'movie'),)
+        index_together = (('user', 'movie'),)
